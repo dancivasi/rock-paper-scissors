b72a5d8c1a64ff3faa8f92b878b3e0c064784970
dancivasi <vasilicavasilica26@yahoo.com>
2024-01-29 00:52:31 +0200

    changes made to the code so it can randomly make 2 person play rock papers scissors and make one of them win + added tdd (3 in total)

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
 import random
 
+CHOICES = ["Rock", "Paper", "Scissors"]
 
-choices = ["Rock", "Paper", "Scissors"]
-class Player():
+
+class Player:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+        self.choice = self.draw()
 
-    def draw(self):
-        return random.choice(choices)
+    @staticmethod
+    def draw():
+        return random.choice(CHOICES)
 
 
+class Game:
+    @staticmethod
+    def decide_winner(player1, player2):
+        if player1.choice == player2.choice:
+            return "Tie"
+        elif player1.choice == "Rock":
+            if player2.choice == "Paper":
+                return f"{player2.name} {player2.surname} Wins"
+            else:
+                return f"{player1.name} {player1.surname} Wins"
+        elif player1.choice == "Paper":
+            if player2.choice == "Scissors":
+                return f"{player2.name} {player2.surname} Wins"
+            else:
+                return f"{player1.name} {player1.surname} Wins"
+        elif player1.choice == "Scissors":
+            if player2.choice == "Rock":
+                return f"{player2.name} {player2.surname} Wins"
+            else:
+                return f"{player1.name} {player1.surname} Wins"
+
+
+if __name__ == "__main__":
+    player1 = Player("Simon", "Vasile")
+    player2 = Player("Adrian", "Moloce")
+
+    game = Game()
+    draw = game.decide_winner(player1, player2)
+
+    while draw == "Tie":
+        player1 = Player("Simon", "Vasile")
+        player2 = Player("Adrian", "Moloce")
+        draw = game.decide_winner(player1, player2)
+
+    print(draw)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,21 @@
-from app import Player, choices
+from app import Player, CHOICES, Game
 import pytest
 
 
+@pytest.fixture()
+def game():
+    player1 = Player("Simon", "Vasile")
+    player2 = Player("Adrian", "Moloce")
+    player1.draw()
+    player2.draw()
+    game = Game()
+    return game.decide_winner(player1, player2)
 
 def test_check_if_playerone_draws_something():
-    player1 = Player("Simon", "Vasile")
-    draw = player1.draw()
-    assert draw in choices
+    assert Player("Simon", "Vasile").draw() in CHOICES
 
 def test_if_playertwo_draws_something():
-    player2 = Player("Moloce", "Adrian")
-    draw = player2.draw()
-    assert draw in choices
+    assert Player("Moloce", "Adrian").draw() in CHOICES
 
+def test_if_game_can_tie_or_win(game):
+    assert game == "Tie" or game == "Simon Vasile Wins" or game == "Adrian Moloce Wins"
